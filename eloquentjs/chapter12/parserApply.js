4b2d9f697e278a3a1018590193ebaa0f3bf14e6a
jschneiderm98 <julio_schneider@hotmail.com>
2021-07-19 09:28:17 -0300

    chapter12 start

---
+++ eloquentjs/chapter12/parserApply.js
@@ -0,0 +1,24 @@
+const { parseExpression, skipSpace } = require('./expressionParser');
+
+function parseApply(expr, program) {
+  program = skipSpace(program);
+  if (program[0] != "(") {
+    return {expr: expr, rest: program};
+  }
+
+  program = skipSpace(program.slice(1));
+  expr = {type: "apply", operator: expr, args: []};
+  while (program[0] != ")") {
+    let arg = parseExpression(program);
+    expr.args.push(arg.expr);
+    program = skipSpace(arg.rest);
+    if (program[0] == ",") {
+      program = skipSpace(program.slice(1));
+    } else if (program[0] != ")") {
+      throw new SyntaxError("Expected ',' or ')'");
+    }
+  }
+  return parseApply(expr, program.slice(1));
+}
+
+module.exports.parseApply = parseApply;
